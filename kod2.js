5638bdbc8a2a9249b8c8494394ce65b5d8724a08
Mert <susxdza@gmail.com>
2026-08-29 17:25:57 +0300

    mert instants dogru

---
+++ kod2.js
@@ -0,0 +1,35 @@
+function sesiCal(){
+    var ses = document.getElementById("Senmalsin");
+    ses.currentTime = 0;
+    ses.play();
+}
+function sesiCal1(){
+    var ses = document.getElementById("mj");
+    ses.currentTime = 0;
+    ses.play();
+}
+function sesiCal2(){
+    var ses = document.getElementById("yetersiz");
+    ses.currentTime = 0;
+    ses.play();
+}
+function sesiCal3(){
+    var ses = document.getElementById("dbgg");
+    ses.currentTime = 0;
+    ses.play();
+}
+function sesiCal4(){
+    var ses = document.getElementById("iki");
+    ses.currentTime = 0;
+    ses.play();
+}
+function sesiCal5(){
+    var ses = document.getElementById("degirmen");
+    ses.currentTime = 0;
+    ses.play();
+}
+    function sesiCal6(){
+    var ses = document.getElementById("mj2");
+    ses.currentTime = 0;
+    ses.play();
+}
